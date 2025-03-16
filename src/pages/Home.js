12117893ec811c5c986d45dc7ee4b8d29a53f71e
zohaibJR <zohaibrehman595@gmail.com>
2025-03-16 06:11:45 +0500

    Updated Contact Page and pushed all changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from "react-router-dom";
-import Banner from '../assets/pizza.jpg'
+// import Banner from '../assets/pizza.jpg'
+import Banner2 from '../assets/Banner2.jpg'
 import '../styles/Home.css'
 
 function Home() {
   return (
-    <div className="home" style={{ backgroundImage: `url(${Banner})` }}>
+    <div className="home" style={{ backgroundImage: `url(${Banner2})` }}>
       <div className="headerContainer" >
         <h1>GameXpress</h1>
         <p>Where Every Game Finds a Player</p>
